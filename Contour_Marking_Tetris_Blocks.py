7c37a00788171b211ef2e250b398943d2f9713c8
lalifala <socialmedia@lalifala.de>
2017-08-26 16:04:59 +0200

    Update

--- Contour_Marking_Tetris_Blocks.py
+++ Contour_Marking_Tetris_Blocks.py
@@ -6,8 +6,8 @@ import cv2
 # load the Tetris block image, convert it to grayscale, and threshold
 # the image
 print("OpenCV Version: {}".format(cv2.__version__))
-image = cv2.imread("D:/AI/ComputerVision/Images/tetris_blocks.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+img = cv2.imread("D:/AI/ComputerVision/Images/tetris_blocks.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
 
 # check to see if we are using OpenCV 2.X
@@ -21,6 +21,6 @@ elif imutils.is_cv3():
                                     cv2.CHAIN_APPROX_SIMPLE)
 
 # draw the contours on the image
-cv2.drawContours(image, cnts, -1, (240, 0, 159), 3)
-cv2.imshow("Image", image)
+cv2.drawContours(img, cnts, -1, (240, 0, 159), 3)
+cv2.imshow("Puzzle Contours", img)
 cv2.waitKey(0)
